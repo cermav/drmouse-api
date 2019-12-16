29865debf27100a803b22710e45f17ec032889f6
petr <petr@ocenasek.cz>
2019-12-16 23:58:07 +0100

    obrazky

--- database/update.sql
+++ database/update.sql
@@ -46,3 +46,78 @@ SELECT
 FROM drmouse_old.doctor_rating AS dr
 WHERE date_created != '0000-00-00 00:00:00'
       AND EXISTS(SELECT id FROM drmouse_old.doctor_voted WHERE doctor_hash = dr.doctor_hash)
+
+
+SELECT REPLACE(avatar, 'https://www.drmouse.cz/new/wp-content/uploads/', '') FROM users
+
+SELECT * FROM users WHERE avatar IS NOT NULL AND avatar LIKE 'http%'
+
+UPDATE users SET avatar = SUBSTRING(avatar, 2) WHERE avatar IS NOT NULL AND SUBSTRING(avatar, 1, 1) = '/'
+
+
+UPDATE users SET avatar = REPLACE(avatar, 'https://www.drmouse.cz/wp-content/uploads/', '')
+UPDATE users SET avatar = REPLACE(avatar, 'https://www.drmouse.cz/new/wp-content/uploads/', '')
+UPDATE users SET avatar = REPLACE(avatar, 'http://www.drmouse.cz/new/wp-content/uploads/', '')
+
+UPDATE users SET avatar = NULL WHERE avatar LIKE 'https://www.drmouse.cz/wp-content/themes/DrMouse2/img/profileDoctor01.png'
+UPDATE users SET avatar = NULL WHERE avatar LIKE 'https://www.drmouse.cz/wp-content/themes/DrMouse2/img/profileDoctor02.png'
+UPDATE users SET avatar = NULL WHERE avatar LIKE 'https://www.drmouse.cz/wp-content/themes/DrMouse2/img/profileDoctor03.png'
+UPDATE users SET avatar = NULL WHERE avatar LIKE 'https://www.drmouse.cz/wp-content/themes/DrMouse2/img/profileDoctor04.png'
+UPDATE users SET avatar = NULL WHERE avatar LIKE 'http://www.drmouse.cz/new/wp-content/themes/DrMouse2/img/profileDoctor02.png'
+UPDATE users SET avatar = NULL WHERE avatar LIKE 'https://www.drmouse.cz/wp-content/themes/DrMouse2'
+
+
+SELECT * FROM users WHERE avatar IS NOT NULL AND avatar LIKE '%/%'
+UPDATE users SET avatar = REPLACE(avatar, 'doctors/', '') WHERE avatar LIKE 'doctors/%'
+
+UPDATE users SET avatar = REPLACE(avatar, '2016/10/', '') WHERE avatar LIKE '2016/10/%';
+UPDATE users SET avatar = REPLACE(avatar, '2016/11/', '') WHERE avatar LIKE '2016/11/%';
+UPDATE users SET avatar = REPLACE(avatar, '2016/12/', '') WHERE avatar LIKE '2016/12/%';
+
+UPDATE users SET avatar = REPLACE(avatar, '2017/01/', '') WHERE avatar LIKE '2017/01/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/02/', '') WHERE avatar LIKE '2017/02/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/03/', '') WHERE avatar LIKE '2017/03/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/04/', '') WHERE avatar LIKE '2017/04/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/05/', '') WHERE avatar LIKE '2017/05/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/06/', '') WHERE avatar LIKE '2017/06/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/07/', '') WHERE avatar LIKE '2017/07/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/08/', '') WHERE avatar LIKE '2017/08/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/09/', '') WHERE avatar LIKE '2017/09/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/10/', '') WHERE avatar LIKE '2017/10/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/11/', '') WHERE avatar LIKE '2017/11/%';
+UPDATE users SET avatar = REPLACE(avatar, '2017/12/', '') WHERE avatar LIKE '2017/12/%';
+
+UPDATE users SET avatar = REPLACE(avatar, '2018/01/', '') WHERE avatar LIKE '2018/01/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/02/', '') WHERE avatar LIKE '2018/02/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/03/', '') WHERE avatar LIKE '2018/03/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/04/', '') WHERE avatar LIKE '2018/04/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/05/', '') WHERE avatar LIKE '2018/05/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/06/', '') WHERE avatar LIKE '2018/06/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/07/', '') WHERE avatar LIKE '2018/07/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/08/', '') WHERE avatar LIKE '2018/08/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/09/', '') WHERE avatar LIKE '2018/09/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/10/', '') WHERE avatar LIKE '2018/10/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/11/', '') WHERE avatar LIKE '2018/11/%';
+UPDATE users SET avatar = REPLACE(avatar, '2018/12/', '') WHERE avatar LIKE '2018/12/%';
+
+
+UPDATE users SET avatar = REPLACE(avatar, '2019/01/', '') WHERE avatar LIKE '2019/01/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/02/', '') WHERE avatar LIKE '2019/02/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/03/', '') WHERE avatar LIKE '2019/03/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/04/', '') WHERE avatar LIKE '2019/04/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/05/', '') WHERE avatar LIKE '2019/05/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/06/', '') WHERE avatar LIKE '2019/06/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/07/', '') WHERE avatar LIKE '2019/07/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/08/', '') WHERE avatar LIKE '2019/08/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/09/', '') WHERE avatar LIKE '2019/09/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/10/', '') WHERE avatar LIKE '2019/10/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/11/', '') WHERE avatar LIKE '2019/11/%';
+UPDATE users SET avatar = REPLACE(avatar, '2019/12/', '') WHERE avatar LIKE '2019/12/%';
+
+
+
+
+
+
+
+
