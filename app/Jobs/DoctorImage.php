3e8d9c4cdd6c18714c814c346b4e7c9d61f2c479
Petr Ocenasek <petr@code8.cz>
2020-03-03 16:37:39 +0100

    Update doctor images which are not existing

---
+++ app/Jobs/DoctorImage.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Doctor;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class DoctorImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        // go throught all doctors
+        foreach (Doctor::all() as $doctor) {
+            try {
+
+                $imageLink = 'https://api.drmouse.code8.link/storage/profile/' . $doctor->user->avatar;
+
+                // Open file
+                $handle = @fopen($imageLink, 'r');
+
+                // Check if file exists
+                if(!$handle){
+                    // update doctor
+                    $doctor->user->update([
+                        'avatar' => null
+                    ]);
+                    echo $imageLink . ' not found';
+                }else{
+                    echo "found";
+                    fclose($handle);
+                }
+
+/*
+                // get image path
+                dd($imageLink);
+
+
+                info($doctor->user->name);
+*/
+            }
+            catch (\Exception $ex) {
+                dd($ex);
+            }
+        }
+    }
+}
