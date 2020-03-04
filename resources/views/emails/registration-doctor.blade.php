ef6b2a3cdab5d7f6bc490458de4860c8f01c8cb3
LukasV <lukas@code8.cz>
2020-03-04 11:52:14 +0100

    Email layout

--- resources/views/emails/registration-doctor.blade.php
+++ resources/views/emails/registration-doctor.blade.php
@@ -2,85 +2,58 @@
 
 @section('title', 'Doctors suggestions')
 @section('content')
-    <p>Dobrý den,</p>
-    <p>na webu drmouse.cz jsem provedli novou registraci. Prosím aktivujte váš účet kliknutím na aktivační link.</p>
-    <p><a href="<?php echo $verify_link ?>"><?php echo $verify_link ?></a></p>
+<p>Dobrý den,</p>
+<p>na webu drmouse.cz jsem provedli novou registraci. Prosím aktivujte váš účet kliknutím na aktivační link.</p>
 
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="400">
-        <tbody>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+    <tbody>
+        <tr>
+            <td style="word-break:break-all;">
+                <a href="<?php echo $verify_link ?>" style="word-break:break-all;"><?php echo $verify_link ?></a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="400">
+    <tbody>
         <tr>
             <th align="left">
                 <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                     <tbody>
-                    <tr>
-                        <th class="align-left">Vaše jméno a příjmení (název kliniky)</th>
-                        <td>{{ $user->name }}</td>
-                    </tr>
-                    <tr>
-                        <th class="align-left">Email</th>
-                        <td>{{ $user->email }}</td>
-                    </tr>
-                    <tr>
-                        <th class="align-left">Ulice a číslo popisné</th>
-                        <td>{{ $doctor->street }}</td>
-                    </tr>
-                    <tr>
-                        <th class="align-left">Město</th>
-                        <td>{{ $doctor->city }}</td>
-                    </tr>
-                    <tr>
-                        <th class="align-left">PSČ</th>
-                        <td>{{ $doctor->post_code }}</td>
-                    </tr>
-                    <tr>
-                        <th class="align-left">Telefonní číslo</th>
-                        <td>{{ $doctor->phone }}</td>
-                    </tr>
+                        <tr>
+                            <th class="align-left">Vaše jméno a příjmení (název kliniky)</th>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <th class="align-left">Email</th>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                        <tr>
+                            <th class="align-left">Ulice a číslo popisné</th>
+                            <td>{{ $doctor->street }}</td>
+                        </tr>
+                        <tr>
+                            <th class="align-left">Město</th>
+                            <td>{{ $doctor->city }}</td>
+                        </tr>
+                        <tr>
+                            <th class="align-left">PSČ</th>
+                            <td>{{ $doctor->post_code }}</td>
+                        </tr>
+                        <tr>
+                            <th class="align-left">Telefonní číslo</th>
+                            <td>{{ $doctor->phone }}</td>
+                        </tr>
                     </tbody>
                 </table>
-            </td>
+                </td>
         </tr>
-        </tbody>
-    </table>
-    </td>
-    </tr>
-    </table>
+    </tbody>
+</table>
 
-    <p>Pokud jste nežádali o registraci v databazi veterinářů Dr. Mouse, pak tento email prosím ignorujte.</p>
-    <p>Pac a pusu,<br>DrMouse</p>
+<br>
+<p>Pokud jste nežádali o registraci v databazi veterinářů Dr. Mouse, pak tento email prosím ignorujte.</p>
+<p>Pac a pusu,<br>DrMouse</p>
 @endsection
-
-<p>Dobrý den,</p>
-
-</td>
-</tr>
-
-<!-- END MAIN CONTENT AREA -->
-</table>
-<!-- END CENTERED WHITE CONTAINER -->
-
-<!-- START FOOTER -->
-<div class="footer">
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
-        <tr>
-            <td class="content-block">
-                <span class="apple-link">www.drmouse.cz</span>
-            </td>
-        </tr>
-        <tr>
-            <td class="content-block powered-by">
-                Dr.Mouse
-            </td>
-        </tr>
-    </table>
-</div>
-<!-- END FOOTER -->
-
-</div>
-</td>
-<td>&nbsp;</td>
-</tr>
-</table>
-
-</body>
-</html>
