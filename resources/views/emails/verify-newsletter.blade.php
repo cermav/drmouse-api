665cc54ca6781eb8b2cb67b32091cdc12cab7dcd
LukasV <lukas@code8.cz>
2020-03-04 14:20:33 +0100

    Not wrapping long link slipped through

--- resources/views/emails/verify-newsletter.blade.php
+++ resources/views/emails/verify-newsletter.blade.php
@@ -4,7 +4,15 @@
 @section('content')
 <p>Dobrý den,</p>
 <p>prosím, potvrďte níže uvedeným linkem vaši emailovou adresu</p>
-<p><a href="<?php echo $verify_link ?>"><?php echo $verify_link ?></a></p>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+    <tbody>
+        <tr>
+            <td style="word-break:break-all;">
+                <a href="<?php echo $verify_link ?>" style="word-break:break-all;"><?php echo $verify_link ?></a>
+            </td>
+        </tr>
+    </tbody>
+</table>
 <p>Pokud jste se neregistrovali k odběru novinek na našich stránkách, ignorujte prosím tento email.</p>
 <p>Pac a pusu,<br>Dr.Mouse</p>
 @endsection
