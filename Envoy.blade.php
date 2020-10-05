be504c4c427a0239cca34a189f1ecf01eb4ef4e2
petr <petr@ocenasek.cz>
2020-10-05 16:29:19 +0200

    Deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,36 +2,32 @@
 
 @setup
     $repository = 'git@gitlab.code8.cz:burt/drmouse-api.git';
-    $releases_dir = '/var/www/drmouse-api/releases';
-    $app_dir = '/var/www/drmouse-api';
-    $release = date('YmdHis');
-    $new_release_dir = $releases_dir .'/'. $release;
-
     $branch = isset($branch) ? $branch : "master";
+    $release = date('YmdHis');
+
+    if ($branch === 'develop') {
+        $app_dir = '/var/www/drmouse-develop/server';
+    } else {
+        $app_dir = '/var/www/drmouse-api';
+    }
+
+    $releases_dir = $app_dir . '/releases';
+    $new_release_dir = $releases_dir .'/'. $release;
 
 @endsetup
 
 @story('deploy')
-    test
-@endstory
-
-@story('deploy_master')
     clone_repository
     run_composer
     update_symlinks
     migrate
 @endstory
 
-@task('test')
-    echo "Deploy branch ({{ $branch }})"
-@endtask
-
-
-
 @task('clone_repository')
+    echo "Deploy branch ({{ $branch }})"
     echo 'Cloning repository'
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+    git clone --branch {{ $branch }} --depth 1 {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
     git reset --hard {{ $commit }}
 @endtask
@@ -48,13 +44,11 @@
     ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
     ln -s {{ $new_release_dir }}/storage/app/public {{ $new_release_dir }}/public/storage
 
-
     echo 'Linking .env file'
     ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
 
     echo 'Setup write permissions'
     chmod -R 777 {{ $new_release_dir }}/bootstrap/cache/
-
 
     echo 'Linking current release'
     ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
