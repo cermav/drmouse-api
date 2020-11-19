1b27f3ba0bff2e81c401f3f71a2532c8ffd3198f
Vitek <vitek@code8.cz>
2020-11-19 11:19:41 +0100

    # model adjust

--- app/Models/PetAppointment.php
+++ app/Models/PetAppointment.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class PetAppointment extends Model
 {
     public $table = "pet_appointments";
-    protected $fillable = ['date', 'description'];
+    protected $fillable = ['date', 'description', 'updated_at', 'created_at'];
 }
