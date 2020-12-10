b52bc6776c1b3a7df7a0d8048e11afda3f955b03
Vitek <vitek@code8.cz>
2020-12-10 12:40:51 +0100

    no message

--- database/migrations/2020_08_04_090625_create_pets_table.php
+++ database/migrations/2020_08_04_090625_create_pets_table.php
@@ -24,6 +24,8 @@ class CreatePetsTable extends Migration
             $table->integer('chip_number')->default(null);
             $table->string('background');
             $table->string('avatar');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
         });
     }
 
