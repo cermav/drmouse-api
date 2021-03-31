4d0001a55034bb7ac14c62919c08442f9e785e65
Vitek <vitek@code8.cz>
2021-03-31 13:20:08 +0200

    # remove folder

--- app/Http/Controllers/Api/PetController.php
+++ app/Http/Controllers/Api/PetController.php
@@ -552,7 +552,7 @@ class PetController extends Controller
             foreach ($files as $file){
                 if (Storage::disk('public')->has('pet_records' . DIRECTORY_SEPARATOR . $record_id . DIRECTORY_SEPARATOR . $file->file_name)) Storage::disk('public')->delete('pet_records' . DIRECTORY_SEPARATOR . $record_id . DIRECTORY_SEPARATOR . $file->file_name);
             }
-            if (Storage::disk('public')->has('app/public/pet_records/'.$record_id)) rmdir(storage_path('app/public/pet_records/'.$record_id));
+            if (Storage::disk('public')->has('app' . DIRECTORY_SEPARATOR .'public' . DIRECTORY_SEPARATOR .'pet_records' . DIRECTORY_SEPARATOR . $record_id)) rmdir(storage_path('app/public/pet_records/'.$record_id));
             Record::where('id', $record_id)->delete();
             return response()->json("Record and its files deleted successfully", JsonResponse::HTTP_OK);
         }
