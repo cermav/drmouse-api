dc007e7a00c256b30834abff89f890c4b06ad6e8
Vitek <vitek@code8.cz>
2021-04-14 13:27:18 +0200

    # err 401 debug

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -13,11 +13,13 @@ class FileController extends Controller
 {
     public function getFileURI($file_id)
     {
+        return Auth::User()->id;
         if (!RecordFile::find($file_id)) return response()->json(
             ['error' => 'Failed to authorize the request'],
             401
         );
-        if (Auth::User()->id != RecordFile::find($file_id)->owner_id || Auth::User()->role_id != UserRole::ADMINISTRATOR) {
+        if (Auth::User()->id != RecordFile::find($file_id)->owner_id || Auth::User()->role_id != UserRole::ADMINISTRATOR) 
+        {
             return response()->json(
                 ['error' => 'Failed to authorize the request.'],
                 401
@@ -33,6 +35,5 @@ class FileController extends Controller
             );
         }
         return Storage::download($path);
-
     }
 }
