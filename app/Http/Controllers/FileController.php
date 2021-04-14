4f347413b7fa20c6d3acaa97e6e501a8f599ed89
Vitek <vitek@code8.cz>
2021-04-14 12:39:12 +0200

    no message

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -14,12 +14,12 @@ class FileController extends Controller
     public function getFileURI($file_id)
     {
         if (!RecordFile::find($file_id)) return response()->json(
-            ['error' => 'Failed to authorize the request.'],
+            ['error' => 'Failed to authorize the request'],
             401
         );
         if (Auth::User()->id !== RecordFile::find($file_id)->owner_id || Auth::User()->role_id != UserRole::ADMINISTRATOR) {
             return response()->json(
-                ['error' => 'Failed to authorize the request.'.Auth::User()->id ." ". RecordFile::find($file_id)->owner_id ],
+                ['error' => 'Failed to authorize the request.'],
                 401
             );
         }
