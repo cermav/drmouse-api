d0899e3672a5f0a649cae489bfec5cdea277ad47
vitek <vitek@code8.cz>
2021-06-07 14:09:50 +0200

    debug cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-     $schedule->job(new PetNotification)->everyTwoMinutes();
+     $schedule->job(new PetNotification()->handle())->everyTwoMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
