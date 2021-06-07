bbe9b8d0b1247647b839692c3f0d763c3a641e5e
vitek <vitek@code8.cz>
2021-06-07 13:47:01 +0200

    test cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Mail\PetNotification;
+use App\Jobs\PetNotification;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,6 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        
+     $schedule->job(new PetNotification)->everyFiveMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
